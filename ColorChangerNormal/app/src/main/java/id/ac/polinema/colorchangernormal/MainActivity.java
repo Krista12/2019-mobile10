a668050af70a5be7728effb6bde6686e64e8c038
Krista12 <krisbelldrn@gmail.com>
2019-11-03 16:06:06 +0700

    Menambahkan library, class untuk menyimpan informasi warna, serta merubah tampilan dari potret ke landscape

--- ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
+++ ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
@@ -2,6 +2,7 @@ package id.ac.polinema.colorchangernormal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -15,23 +16,36 @@ public class MainActivity extends AppCompatActivity {
 	private ConstraintLayout rootView;
 	private Button btnChangeColor;
 
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		final ConstraintLayout rootView;
+		Button btnChangeColor;
+		// Tambahkan ViewModel
+		final ColorViewModel colorViewModel;
+		colorViewModel = ViewModelProviders.of(this).get(ColorViewModel.class);
 		setContentView(R.layout.activity_main);
 		rootView = findViewById(R.id.layout_main);
 		btnChangeColor = findViewById(R.id.change_color);
 		
-		rootView.setBackgroundColor(generateRandomColor());
+		//rootView.setBackgroundColor(generateRandomColor());
+
+		rootView.setBackgroundColor(colorViewModel.getColor());
 
 		// Tambahkan event klik pada tombol
 		btnChangeColor.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				int color = generateRandomColor();
+				colorViewModel.setColor(color);
 				rootView.setBackgroundColor(color);
 			}
 		});
+
+
+
+
 	}
 
 	private int generateRandomColor(){
